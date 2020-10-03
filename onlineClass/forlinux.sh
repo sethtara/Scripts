32468f4dd3f003df5eec93f39f166a062608578e
sethtara <tarakantseth@protonmail.com>
2020-10-03 09:14:47 +0545

    updated the links of online classes

--- onlineClass/forlinux.sh
+++ onlineClass/forlinux.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 browser="iridium-browser"
-cn="https://meet.google.com/iin-szgx-qij"
+cn="https://meet.google.com/ecr-dosb-tnd"
 flat="https://meet.google.com/nwz-drot-dig"
-ppl="https://meet.google.com/cyr-nnmw-zre"
+ppl="https://meet.google.com/xra-jgta-ros"
 dppm="https://meet.google.com/yzj-nqxa-tuw"
-sl="https://meet.google.com/iym-irpf-ckn"
+sl="https://meet.google.com/ecu-dmda-beo"
 se="http://meet.google.com/sdz-mbup-wma"
 es="https://meet.google.com/kah-ndgu-jfu"
 echo "kun class ho :"
